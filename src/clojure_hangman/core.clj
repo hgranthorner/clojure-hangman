c24069b555e72ddc3145eaf01778cb6418a12855
Grant Horner <h.grant.horner@gmail.com>
2020-08-20 20:27:12 -0400

    got it

--- src/clojure_hangman/core.clj
+++ src/clojure_hangman/core.clj
@@ -1,34 +1,28 @@
 (ns clojure_hangman.core
-  (:require [seesaw.core :as ss]))
-
-(defn foo []
-  (println "Hello the world"))
+  (:require [seesaw.core :as ss]
+            [seesaw.bind :as ssb]))
 
 (def state {:count (atom 1)})
 
-(defn items [state]
-  [(ss/text (str @(:count state)))
-   (ss/button
-     :text "Click Me!"
-     :listen [:action #(ss/repaint! (ss/select (ss/to-root %) [:*]))])
-   (ss/button
-     :text "Inc"
-     :listen [:action (fn [_]
-                        (swap! (:count state) inc)
-                        (println @(:count state)))])])
-
-(defn window [state]
+(defn frame-component [label button]
   (ss/frame
-    :title "First Example"
+    :title "Test"
+    :size [200 :by 200]
     :content (ss/flow-panel
                :align :center
-               :items (items state))
-    :width 200
-    :height 200))
+               :items [label button])))
 
-(defn run []
-  (ss/show! (window state)))
+(defn run [count]
+  (let [l (ss/label :text (str @count))
+        inc (ss/button :text "Increase" :listen [:action (fn [_] (swap! count inc))])
+        frame (frame-component l inc)]
+    (ssb/bind
+      count
+      (ssb/property l :text))
+    (ss/show! frame))
+  ;(ss/show! (window state))
+  )
 
 (defn -main
   [& _]
-  (run))
+  (run (:count state)))
